31cfd52f58c4c45defc94e7ebc3e32fff506e608
Your Name <you@example.com>
2023-06-07 21:19:02 +1000

    Add tracking popups and related functionality

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 # jeli2782-tracker
-Jeli2782: Asessment 3 Submission for DECO2017
+##Jeli2782: Asessment 3 Submission for DECO2017
+
+###RunMate is a single-page tracking web application that enables users to monitor their running performance and habits, as well as reflect on their past runs. I designed and coded this throughout semester 2, covering both front-end and back-end aspects. 
+
+I chose to design my project around Fitness, as running is one of my personal hobbies and something I have always been fascinated in my progress of. My goals for this project was to enable users to have a place to monitor their progress in the intensity, distance and energy burned on their runs. This also would motivate users to continue their journey within health and fitness by viewing their improvements over time. There are also personal elements to the web application, such as personal reflects and post-run pictures the user can upload - all so that the application feels like it is monitering their personal progress.
+
+###Main features: 
+- Introduction: 
+    - Introduction into RunMate, what it does.
+    - Introduction into the different categories it tracks progress in: Intensity of workout, Distance covered per run, Calories Burned.
+
+- Tracking:
+    - Fill out forms around Intensity of workout, Distance covered per run, Calories Burned.
+    - Personalise tracking and use of RunMate by allowing users to create titles, reflect on their run in a journal-style space and upload pictures/selfies of themselves after their run or the space they ran.
+- View Run Logs: 
+    - Reflect on and monitor progress by clicking on and viewing past runs and the Intensity of workout, Distance covered per run, Calories Burned, reflections and pictures etc. 
+    - Swipe through run history in a carousel swipe-through style
+- View Progress:
+    - View progress of individual categories: 
+        -  Intensity of workout, over time
+        -  Distance covered per run, over time
+        - Calories Burned, over time
+
+
+###My initial code was done on Replit, as I found it easier to edit my code and see my changes as I went. 
 
 Using the Date Picker:
 - Learned to use a date picker library and how to include corresponding css and javascript files to html for styles and functionality.
 - html was modified so that users could not only see today's date, but also have the ability to edit it to create a log entry from a previous date.
 - Javascript initialisation code inserted to initialise the date picker library and attach it to the date input field. 
-- 
+
+###Installation:
+
+To set up the project locally, follow these steps: 
+
+####Requirements: 
+- Visual Studio Code (or editor of choice)
+- have GIT installed on your system
+- have node.js and npm installed on your system
+
+####Steps: 
+- open a terminal 
+- clone the repository using the following command: 
--- public/script.js
+++ public/script.js
@@ -1,209 +1,143 @@
 //Create a new log entry
-function createLogEntry(){
+  function createLogEntry(){ 
     //user input values - retrieve variables
     //change all variables to constants
-    const titleInput =
-      document.getElementById("title").value;
+    const titleInput = document.getElementById("title");
+    const dateInput=document.getElementById("date");
     const intensityInput=document.getElementById("intensity");
     const distanceInput=document.getElementById("distance");
     const caloriesInput=document.getElementById("calories");
     const ratingInput=document.getElementById("rating-text");
-    const selfieInput=document.getElementById('selfie');
+    const selfieInput=document.getElementById("selfie");
+
     //get selected values from tracking section
-    const title=titleInput.value;
-    const intensity=intensityInput.value;
-    const distance=distanceInput.value;
-    const calories=caloriesInput.value;
-    const rating=ratingInput.value;
-    const date=dateInput.value;
+      const title = titleInput.value;
+      const intensity = intensityInput.value;
+      const distance = distanceInput.value;
+      const calories = caloriesInput.value;
+      const rating = ratingInput.value;
 
+      //create a new log entry with provided data 
+    const logEntry=document.createElement("div");
+    logEntry.className = 'log-entry';
 
-  //Create a new log entry in a box named log-entry with title, intensity, distance, calories, date, rating and photo if chose to upload
+    const logContent = document.createElement("div");
+    logContent.classList.add("log-content");
+    logEntry.appendChild(logInfo);
 
-  const logEntry=document.createElement('div');
-    logEntry.className='log-entry';
+    const logDate=document.createElement("p");
+    const currentDate = newDate().toISOString().split("T")[0];
+    logDate.textContent = dateInput.value || currentDate;
+    logEntry.appendChild(logDate);
 
-    const dateElement=document.createElement('p');
-    dateElement.textContent= 'Date:' + date;
-    logEntry.appendChild(dateElement);
-
-    document.addEventListener("DOMContentLoaded", function(){
-      var dateField = document.getElementById("date");
-      new DatePicker(dateField,{
-        minDate: new Date(new Date().setMonth(new Date().getMonth() - 1))
-      });
-    });
-
-    const titleElement=document.createElement('h4');
-    titleElement.textContent='title' + title;
+    const titleElement=document.createElement("h4");
+    titleElement.textContent= title;
     logEntry.appendChild(titleElement);
 
-    const intensityElement=document.createElement('p');
-    intensityElement.textContent= 'Intensity:' + intensity;
+    const intensityElement=document.createElement("p");
+    intensityElement.textContent= "Intensity:" + intensity;
     logEntry.appendChild(intensityElement);
     
   //Create string for Distance name, value, measurement
-    const distanceElement=document.createElement('p');
-    distanceElement.textContent= 'Distance:' + distance + 'km';
+    const distanceElement=document.createElement("p");
+    distanceElement.textContent= "Distance:" + distance + "km";
     logEntry.appendChild(distanceElement);
 
   //Create string for Calories name, value, measurement name
-    const caloriesElement=document.createElement('p');
-    caloriesElement.textContent= 'Calories:' + calories + 'kCal';
+    const caloriesElement=document.createElement("p");
+    caloriesElement.textContent= "Calories:" + calories + "kCal";
     logEntry.appendChild(caloriesElement);
 
-    const ratingElement=document.createElement('p');
-    ratingElement.textContent= 'Rating:' + rating;
+    const ratingElement=document.createElement("p");
+    ratingElement.textContent= "Rating:" + rating;
     logEntry.appendChild(ratingElement);
 
-    //Display selfie
-    const reader=new FileReader();
-    reader.onload=function(event){
-      const selfieElement=document.createElement('img');
-      selfieElement.src=event.target.result;
-      selfieElement.alt='Post-run Selfie!'
-//append selfieElement to logEntry using appendChild() method.
-    logEntry.appendChild(selfieElement);
-    };
-  //read data url of file input using 'readAsDataURL() - converting file into data url representation.
-    reader.readAsDataURL(selfieInput.files[0]);
-//append logEntry element to logContainer element using appendChild() method. Adding logEntry and selfieElement to logContainer
-    const logContainer=document.getElementById('log-container');
-    logContainer.appendChild(logEntry);
+    //Process and Display selfie
+    //update progress ection
+   const reader = new FileReader();
+   reader.onload = function(event){
+    const selfieElement = document.createElement("img");
+      selfieElement.src = event.target.result;
+      selfieElement.alt = "Selfie";
+      logEntry.appendChild(selfieElement)
+   };
+   reader.readAsDataURL(selfieInput.files[0]);
 
+   const logContainer = document.getElementById("log-container");
+   logContainer.appendChild(logEntry);
+  
     //removed event listeners for nav links as already done in html
-  
     //CALCULATE averages for the previous week and current week.
-  
     //Clear input fields
 
-    titleInput.value='';
-    dateInput.value='select';
-    distanceInput.value='';
-    caloriesInput.value='';
-    ratingInput.value='';
-    selfieInput.value='';
+    titleInput.value=" ";
+    intensityInput.value="select";
+    distanceInput.value=" ";
+    caloriesInput.value=" ";
+    ratingInput.value=" ";
+    selfieInput.value=" ";
 
-
- 
+    //create log message to console indicating whether the run was completed indoors or outdoors when the user presses outdoor run vs indoor run
+  const indoorRun = document.getElementById("indoorRun");
+  indoorRun.addEventListener("click", function())
   }
 
-  function calculateAverages(){
-    //retreive all log entries
-    const logEntries=document.getElementsByClassName('log-entry');
+//get popup box element
+const intensityBoxContent = document.querySelector(".intensity-box-content");
+const distanceBoxContent = document.querySelector(".distance-box-content");
+const caloriesBoxContent = document.querySelector(".calories-box-content");
 
-    //Initialise variables for the current week / previous week
-    let currentWeek={
-      intensitySum:0,
-      distanceSum:0,
-      caloriesSum:0,
-      ratingSum:0,
-      count:0,
-    };
-    let previousWeek={
-      intensitySum:0,
-      distanceSum:0,
-      caloriesSum:0,
-      ratingSum:0,
-      count:0,
-    };
-  
-//retrieve current date
-const currentDate = new Date();
-//for loop to iterate through each log entry in 'logs' section
-for (let i=0; i < logEntries.length;i++){
-  const logEntry = logEntries[i];
-  const logDate = new Date(logEntry.dataset.logDate);
+//Add event listener for view tracking button - Intensity: 
+const viewTrackingButton = document.getElementById("toggle-intensity");
+viewTrackingButton.addEventListener("click", function(){
+  //show Intensity overlay popup
+  const intensityOverlay = document.getElementById("intensity-overlay");
+  intensityOverlay.classList.remove("hidden");
+});
 
-  //calculate difference between log dates and current date
-const diffDays = Math.floor((currentDate - logDate)/(1000*60*60*24));
+//Initially, hide the content inside 'intensity-popup'. when the user clicks on the "toggle intensity" button, it will toggle the visibility of the content by adding/removing hidden class
+const toggleIntensityButton=document.getElementById("toggle-intensity");
+const intensityOverlay = document.getElementById(".intensity-overlay");
+const closeIntensityButton = document.getElementById("close-intensity");
 
-//Update the corresponding week object based on the difference in days
-if (diffDays <=7){
-  const intensity = parseInt(logEntry.querySelector('.intensity').textContent.split(' : ')[1]);
-  const distance = parseInt(logEntry.querySelector('.distance').textContent.split(' : ')[1]);
-  const calories = parseInt(logEntry.querySelector('.calories').textContent.split(' : ')[1]);
+//add event listener to close button inside intensity popup
+closeIntensityButton.addEventListener("click", function(){
+//hide intesntiy overlay
+intensityOverlay.classList.add("hidden");
+});
 
-  currentWeek.intensitySum+=intensity;
-  currentWeek.distanceSum+=distance;
-  currentWeek.caloriesSum+=calories;
-  currentWeek.count++;
+intensityBoxContent.addEventListener("click", function(){
+  popup-intensity.classList('active');
+});
 
-} else if (diffDays>7 && diffDays <=14){
-  const intensity = parseInt(logEntry.querySelector('.intensity').textContent.split(' : ')[1]);
-  const distance = parseInt(logEntry.querySelector('.distance').textContent.split(' : ')[1]);
-  const calories = parseInt(logEntry.querySelector('.calories').textContent.split(' : ')[1]);
-
-  previousWeek.intensitySum+=intensity;
-  previousWeek.distanceSum+=distance;
-  previousWeek.caloriesSum+=calories;
-  previousWeek.count++;
-  }
+function toggleIntensityButton(){
+  const overlay = document.getElementById("intensity-overlay");
+  overlay.classList.toggle("hidden");
 }
 
-//calculate averages for the current week and previous week
-const currentWeekIntensityAverage = currentWeek.count > 0 ? currentWeek.intensitySum / currentWeek.count :0;
-const currentWeekDistanceAverage = currentWeek.count > 0 ? currentWeek.intensitySum / currentWeek.count :0;
-const currentWeekCaloriesAverage = currentWeek.count > 0 ? currentWeek.intensitySum / currentWeek.count :0;
-
-const previousWeekIntensityAverage = currentWeek.count > 0 ? previousWeek.intensitySum / previoustWeek.count :0;
-const previousWeekDistanceAverage = currentWeek.count > 0 ? previousWeek.intensitySum / previousWeek.count :0;
-const previousWeekCaloriesAverage = currentWeek.count > 0 ? previousWeek.intensitySum / previousWeek.count :0;
-
-//update averages in progression section
-
-document.getElementById('intensity-average').textContext = currentWeekIntensityAverage.toFixed(2);
-document.getElementById('intensity-previous').textContext = previousWeekIntensityAverage.toFixed(2);
-
-document.getElementById('distance-average').textContext = currentWeekDistanceAverage.toFixed(2);
-document.getElementById('distance-previous').textContext = previousWeekDistanceAverage.toFixed(2);
-
-document.getElementById('calories-average').textContext = currentWeekCaloriesAverage.toFixed(2);
-document.getElementById('calories-previous').textContext = previousWeekCaloriesAverage.toFixed(2);
-  }
-
-//create event handler
-
-     //create event handler for the create log button
-     document.getElementById ('create-log').addEventListener('click',function(event));
-    //prevent form submission
-     event.preventDefault();
-     createLogEntry();
-
-//calculate averages function is called when the page is loaded to displau intiial average values
-window.onload = function (){
-  calculateAverages();
-}
-  //setting global variable and initialising the current slide index
-  let currentSlide = 0;
-  
-  function slideCarousel(direction){
-    const carousel = document.querySelector(".carousel");
-    const carouselItems=carousel.querySelectorAll(".carousel-itemactive");
-    const totalSlides = carouselItems.length;
-    
-  //if statements to control left and right arrows. If left button is pressed, currentSlide would become currentSlide - slide. If right button is pressed, currentSlide would become currentSlide + slide.
-    
-    if (direction ==='left'){
-  currentSlide--;
-      if (current< 0){
-        currentSlide=totalSlides - 1;
-      }
-    } else if (direction==='right'){
-    currentSlide++;
-    if (currentSlide === totalSlides){
-      currentSlide = 0;
-    }
-  }
-
-  //get width of carousel item-active
-  const carouselWidth = carouselItems[0].offsetWidth;
-  // calculate translateX value to move the carousel horizontally
-  const translateX = currentSlide * -carouselWidth;
-  // apply translateX transformation to carousel section
-  carousel.style.transform = 'translateX(${translateX}px)';
+function hidePopup(){
+  popupIntensity.classlist.remove("active");
 }
 
-  
-  viewIntensityBtn.addEventListener("click", () => {intensityPopup.style.display = "block";
-  });
+//call toggleintensityContent() function when the button is clicked
+const viewIntensityButton = document.getElementById("toggle-intensity");
+viewIntensityButton.addEventListener("click", toggleIntensityContent);
+
+//submit create log button and output from tracking section to log section
+document.getElementById("create-log").addEventListener("click",createLogEntry);
+
+document.addEventListener("DOMContentLoaded",function(){
+  const viewTrackingButton = document.getElementById("toggle-intensity");
+  const intensityOverlay = document.getElementById("intensity-overlay");
+
+  viewTrackingButton.addEventListener("click",function(){
+    intensityOverlay.classList.toggle("hidden");
+  })
+});
+//submit create log button and output from tracking section to log section
+//event handler for create log button
+document.getElementById("create-log").addEventListener("click", function(event){
+  event.preventDefault();
+  //prevent form from being submitted
+  createLogEntry();
+});
